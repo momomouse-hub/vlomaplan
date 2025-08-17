2633f2b1a5d63108d6560160179c2b27c350e40c
momomouse-hub <allstartsat0226@gmail.com>
2025-08-17 22:14:03 +0900

    chore(lint): apply RuboCop fixes project-wide

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -5,10 +5,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:5173', 'http://127.0.0.1:5173'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options],
-      expose: %w[X-Visitor-Token],
-      credentials: false,
-      max_age: 600
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options],
+             expose: %w[X-Visitor-Token],
+             credentials: false,
+             max_age: 600
   end
 end
